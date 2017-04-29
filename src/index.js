a57beefc90255e9c8c55a2b07fa057d3d146356e
William Chen <wchen@macpro0056.medfusion.local>
2017-04-28 21:13:25 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Main extends React.Component {
+	render() {
+		return <div></div>
+	}
+}
+
+ReactDOM.render(<Main />, document.getElementById('app'));
+
+if (module.hot) {
+	module.hot.accept();
+}
