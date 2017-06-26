699a65d34d6d8938f1e4b9ac06e22cb84c9a9ee5
William Desktop <chenweirongduke@gmail.com>
2017-06-25 23:02:47 -0400

    readded webpack and changes for prod build

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import NoteBoard from './components/NoteBoard';
 import './styles/main.scss';
 
-class Main extends React.Component {
-	render() {
-		return <div>
+const Main = () => {
+	return (
+		<div>
 			<NoteBoard />
 		</div>
-	}
+	)
 }
 
 ReactDOM.render(<Main />, document.getElementById('app'));
